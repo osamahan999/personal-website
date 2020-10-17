8dae65f717e68482013286c2ef864a6639b0658e
osama <hanhan.osama@gmail.com>
2020-10-16 23:24:16 -0700

    initial commit for personal website

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+/**
+ * TODO: split different pages into different files. Then, import said files.
+ */
+
+
 
 function App() {
+
+  const [conditional, setConditional] = useState(0);
+
+
+  const setPageState = () => {
+    if (conditional == 0) return homePage();
+
+    else if (conditional == 1) return projectsPage();
+  }
+
+
+  /**
+   * Returns the homepage
+   */
+  const homePage = () => {
+
+    return (
+      <div>
+        {buttons()}
+
+      </div>
+    )
+
+  }
+
+  /**
+   * Returns the projects page
+   */
+  const projectsPage = () => {
+
+    return (
+      <div>
+        {buttons()}
+
+      </div>
+    )
+
+  }
+
+
+  /**
+   * Returns the state buttons
+   */
+  const buttons = () => {
+    return (
+      <div>
+        <button onClick={() => setConditional(0)} className="object-styling text-class" >Home</button>
+        <button onClick={() => setConditional(1)} className="object-styling text-class" >Projects</button>
+        <button onClick={() => setConditional(2)} className="object-styling text-class" >Random Skills</button>
+      </div>
+    )
+  }
+
+
+
+  /**
+   * Main return statement 
+   */
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      {setPageState()}
+
+
     </div>
   );
 }
+
+
 
 export default App;
